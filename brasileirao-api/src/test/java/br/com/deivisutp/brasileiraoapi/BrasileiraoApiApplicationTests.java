179f00192a8b13a27fc2bfc5d4ec9374c61fb860
Deivis <deivisutp@gmail.com>
2021-10-08 14:58:42 -0300

    first commit

---
+++ brasileirao-api/src/test/java/br/com/deivisutp/brasileiraoapi/BrasileiraoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deivisutp.brasileiraoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
