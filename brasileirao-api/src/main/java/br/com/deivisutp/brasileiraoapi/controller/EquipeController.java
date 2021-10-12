9824607104c4df0b6be0c50c4eeefd82322f7216
Deivis <deivisutp@gmail.com>
2021-10-11 21:27:53 -0300

    get services api

--- brasileirao-api/src/main/java/br/com/deivisutp/brasileiraoapi/controller/EquipeController.java
+++ brasileirao-api/src/main/java/br/com/deivisutp/brasileiraoapi/controller/EquipeController.java
@@ -1,6 +1,17 @@
 package br.com.deivisutp.brasileiraoapi.controller;
 
+import br.com.deivisutp.brasileiraoapi.dto.EquipeResponseDTO;
+import br.com.deivisutp.brasileiraoapi.entities.Equipe;
+import br.com.deivisutp.brasileiraoapi.exception.StandardError;
+import br.com.deivisutp.brasileiraoapi.service.EquipeService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +19,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/equipes")
 public class EquipeController {
+
+    @Autowired
+    private EquipeService equipeService;
+
+    @ApiOperation(value = "Buscar equipe por id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = Equipe.class),
+            @ApiResponse(code = 400, message = "Bad request", response = StandardError.class),
+            @ApiResponse(code = 401, message = "Unauthorized", response = StandardError.class),
+            @ApiResponse(code = 403, message = "Forbidden", response = StandardError.class),
+            @ApiResponse(code = 404, message = "Not found", response = StandardError.class),
+            @ApiResponse(code = 500, message = "Internal server error", response = StandardError.class)
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Equipe> buscarEquipeId(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(equipeService.buscarEquipeId(id));
+    }
+
+    @ApiOperation(value = "Listar equipes")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = EquipeResponseDTO.class),
+            @ApiResponse(code = 400, message = "Bad request", response = StandardError.class),
+            @ApiResponse(code = 401, message = "Unauthorized", response = StandardError.class),
+            @ApiResponse(code = 403, message = "Forbidden", response = StandardError.class),
+            @ApiResponse(code = 404, message = "Not found", response = StandardError.class),
+            @ApiResponse(code = 500, message = "Internal server error", response = StandardError.class)
+    })
+    @GetMapping
+    public ResponseEntity<EquipeResponseDTO> listarEquipes() {
+        return ResponseEntity.ok().body(equipeService.listarEquipes());
+    }
 }
--- brasileirao-api/src/main/java/br/com/deivisutp/brasileiraoapi/service/EquipeService.java
+++ brasileirao-api/src/main/java/br/com/deivisutp/brasileiraoapi/service/EquipeService.java
@@ -1,7 +1,27 @@
 package br.com.deivisutp.brasileiraoapi.service;
 
+import br.com.deivisutp.brasileiraoapi.dto.EquipeResponseDTO;
+import br.com.deivisutp.brasileiraoapi.entities.Equipe;
+import br.com.deivisutp.brasileiraoapi.exception.NotFoundException;
+import br.com.deivisutp.brasileiraoapi.repository.EquipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EquipeService {
+
+    @Autowired
+    private EquipeRepository equipeRepository;
+
+    public Equipe buscarEquipeId(Long id) {
+        return equipeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Nenhuma equipe encontrada com o id informado: " + id));
+    }
+
+    public EquipeResponseDTO listarEquipes() {
+        EquipeResponseDTO equipes = new EquipeResponseDTO();
+        equipes.setEquipes(equipeRepository.findAll());
+
+        return equipes;
+    }
 }
